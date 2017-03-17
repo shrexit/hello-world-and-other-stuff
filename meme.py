d6729b429c87ee302b82b40131110e6ac47cdb54
shrexit <13pearcesam@bartholomew.oxon.sch.uk>
2017-03-17 09:32:52 +0000

    Add files via upload

---
+++ meme.py
@@ -0,0 +1,9 @@
+Python 3.4.1 (v3.4.1:c0e311e010fc, May 18 2014, 10:38:22) [MSC v.1600 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> print ("hello world")
+hello world
+>>> print ("p l e a s e  n o")
+p l e a s e  n o
+>>> print ("k i l l")
+k i l l
+>>> 
